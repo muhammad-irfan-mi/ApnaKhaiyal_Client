212944e76c6f4e05ec8bae35d01ef5e9dc63ef22
muhammad-irfan-mi <muhammadirfan786980@gmail.com>
2025-05-27 18:00:14 +0500

    changes

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,25 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
+const nodeBuiltIns = [
+  'fs',
+  'path',
+  'crypto',
+  'stream',
+  'buffer',
+  'util',
+  'url',
+  'http',
+  'https',
+  'os',
+  'zlib'
+]
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
     rollupOptions: {
-      external: ['fs', 'path', 'crypto']
+      external: nodeBuiltIns
     }
   }
 })
