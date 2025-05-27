195f0f40cc4bde171185e71da5400c840ce6b8de
muhammad-irfan-mi <muhammadirfan786980@gmail.com>
2025-05-27 18:05:02 +0500

    refactor: remove unused node built-ins from Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,25 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-const nodeBuiltIns = [
-  'fs',
-  'path',
-  'crypto',
-  'stream',
-  'buffer',
-  'util',
-  'url',
-  'http',
-  'https',
-  'os',
-  'zlib'
-]
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      external: nodeBuiltIns
-    }
-  }
+  plugins: [react(),tailwindcss()],
 })
